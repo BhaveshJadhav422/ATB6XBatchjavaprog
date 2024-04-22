8961a5f5a655a49d23cdbb6874ec529baf911283
Bhavesh <jadhavbhavesh422@gmail.com>
2024-04-22 11:19:14 +0530

    24 APR 2024

---
+++ src/Ex_110422024/Lab_02.java
@@ -0,0 +1,9 @@
+package Ex_110422024;
+
+public class Lab_02 {
+    public static void main(String[] args) {
+        System.out.println("Hello Bhavesh");
+        System.out.print("Hello Jadhav\n");
+        System.out.print("Hello Neelima");
+    }
+}
