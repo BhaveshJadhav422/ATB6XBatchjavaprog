1d2600b152da14a379c77d3dddf57e0ffcb2ae2e
BhaveshJadhav422 <165446091+BhaveshJadhav422@users.noreply.github.com>
2024-04-24 01:50:42 +0530

    Delete Lab_007.java

--- Lab_007.java
+++
@@ -1,9 +0,0 @@
-package Ex_110422024;
-
-public class Lab_007 {
-    public static void main(String[] args) {
-        System.out.println("9 x 1 = 9");
-        System.out.println("9 x 2 = 18");
-        System.out.println("9 x 10 = 90");
-    }
-}
