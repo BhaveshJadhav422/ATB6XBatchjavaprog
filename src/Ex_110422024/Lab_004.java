e01496e6e2560d831c8cd374e5da87c0be502196
Bhavesh <jadhavbhavesh422@gmail.com>
2024-04-24 02:16:24 +0530

    24 APR 2024

--- src/Ex_110422024/Lab_004.java
+++ src/Ex_110422024/Lab_004.java
@@ -2,7 +2,7 @@ package Ex_110422024;
 
 public class Lab_004 {
     public static void main(String[] args) {
-        int age=24;https://github.com/BhaveshJadhav422/ATB6XBatchjavaprog/
+        int age=24;
         System.out.println(age);
         age=25;
         System.out.println(age);
